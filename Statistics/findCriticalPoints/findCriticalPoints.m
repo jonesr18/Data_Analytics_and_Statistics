406f4f6803b390194ccef44b9a4d82cc32ec4383
Ross Jones <jonesr18@gmail.com>
2014-11-17 16:42:13 -0500

    Added midpoints finding ability

--- Statistics/findCriticalPoints/findCriticalPoints.m
+++ Statistics/findCriticalPoints/findCriticalPoints.m
@@ -1,36 +1,75 @@
-function points = findCriticalPoints(curve, type)
+function varargout = findCriticalPoints(curve, varargin)
     % Curve is the set of points you wish to find the local minima or maxima over
-    % type is a string stating 'min', 'max', or 'both'
-    % - if 'both', the function will return a cell array where points{1} === the maxPoints 
-    %   array and points{2} === the minPoints array. Otherwise the function will directly
-    %   return the vector of points. The points are given as indexes corresponding with
-    %   minumum and/or maxumum local values.
+    % The optional argument specifies what points to return
+    % - 'max': local maxima (default)
+    % - 'min': local minima 
+    % - 'mid': points closest to the average value
+    % - 'all': all options
+    % - If multiple points are requested, the function will return a cell array where 
+    %   each element is the vector of points of a requested type in the order requested. 
+    %    o eg:
+    %      points = findCriticalPoints(curve, 'max', 'min')
+    %      [maxPoints, minPoints] = points{:};
+    % - If a single type of point is requested, the function will directly
+    %   return the vector of points. 
+    % - The points are given as indexes corresponding with minumum and/or maxumum local values.
+    
+    % Default
+    type = {'max'};
+    allTypes = {'max', 'min', 'mid'};
     
     % Check inputs
-    type = lower(type);
     validateattributes(curve, {'numeric'}, {'1D'}, mfilename, 'curve', 1);
-    validatestring(type, {'min', 'max', 'both'}, mfilename, 'type', 2);
+    if nargin > 1
+        type = cell(1, numel(varargin));
+    end
+    for i = 1:numel(varargin)
+        arg = lower(varargin{i});
+        validatestring(arg, {'max', 'min', 'mid', 'all'}, mfilename, 'varargin', i + 1);
+        if strcmpi(arg, 'all')
+            type = allTypes;
+            break
+        end
+        type{i} = arg;
+    end
     
-    % Differentiate curve to find where dCurve = 0
-    dCurve = diff(curve);
+    % Differentiate if finding min/max points
+    if any(strcmpi(type, 'min')) || any(strcmpi(type, 'max'))
+        
+        % Differentiate curve to find where dCurve = 0
+        dCurve = diff(curve);
+
+        % Normalize dCurve to be +/- 1 or NaN if 0.
+        dcNorm = dCurve ./ abs(dCurve);
+        criticalPoints = diff(dcNorm);
+    end
     
-    % Normalize dCurve to be +/- 1 or NaN if 0.
-    dcNorm = dCurve ./ abs(dCurve);
-    criticalPoints = diff(dcNorm);
+    % Bisect if finding mid points
+    if any(strcmpi(type, {'mid'}))
+        
+        % Adjust curve by mean value
+        midCurve = curve - mean(curve);
+        
+        % Normalize midCurve to be +/- 1 or NaN if 0.
+        mcNorm = midCurve ./ abs(midCurve);
+        criticalMid = diff(mcNorm);
+    end
     
     % Find requested points and build output
-    switch type
-        case 'min'
-            minPoints = find(criticalPoints == 2) + 1;     % The +1 adjusts for the diff funcs used
-            points = minPoints;
-        case 'max'
-            maxPoints = find(criticalPoints == -2) + 1;
-            points = maxPoints;
-        case 'both'
-            minPoints = find(criticalPoints == 2) + 1;
-            maxPoints = find(criticalPoints == -2) + 1;
-            points = {maxPoints, minPoints};
-        otherwise
-            error('Type is not correct somehow')
+    varargout = cell(1, numel(type));
+    for i = 1:numel(type)
+        switch type{i}
+            case 'max'
+                maxPoints = find(criticalPoints == -2) + 1;
+                varargout{i} = maxPoints;
+            case 'min'
+                minPoints = find(criticalPoints == 2) + 1; % The +1 adjusts for the diff funcs used
+                varargout{i} = minPoints;
+            case 'mid'
+                midPoints = find(abs(criticalMid) == 2);
+                varargout{i} = midPoints;
+            otherwise
+                error('Type is not correct somehow')
+        end
     end
 end
